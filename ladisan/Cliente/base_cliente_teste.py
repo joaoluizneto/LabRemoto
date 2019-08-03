20f68ccd8bb52775dadd6bb33e722383d68d5596
João Luiz Neto <joao_luiz@id.uff.br>
2019-08-03 00:17:51 -0300

    Controle de processos com script shell

--- ladisan/Cliente/base_cliente_teste.py
+++ ladisan/Cliente/base_cliente_teste.py
@@ -3,26 +3,28 @@ import json
 
 def cria_cliente(ip, porta):
 	"""
-	Criamos o socket e o conectamos ao servidor 
-	"""	
+	Criamos o socket e o conectamos ao servidor
+	"""
 	ip = str(ip)
 	global sockobj
 	sockobj = socket(AF_INET, SOCK_STREAM)
 	sockobj.connect((ip, porta))
-	
 
-cria_cliente('127.0.0.1', 50007)
+try:
+	cria_cliente('127.0.0.1', 50007)
+	print(" Conexão socket com o RaspberryPi estabelecida! ")
+	while True:
 
-while True:
+		#requisita = ''.encode()
+		#sockobj.send(requisita)
 
-	#requisita = ''.encode()	
-	#sockobj.send(requisita)
-	
-	#Depois de mandar uma mensagem esperamos uma resposta do servidor 
-	data = sockobj.recv(1024)
-	json_da_lista = data.decode("utf-8")
-	lista_de_linhas = json.loads(json_da_lista)
-	linhas = '\n'.join(lista_de_linhas)
-	arquivo = open('file2.txt', 'w')
-	arquivo.writelines(linhas)
-	arquivo.close()
+		#Depois de mandar uma mensagem esperamos uma resposta do servidor
+		data = sockobj.recv(1000000)
+		json_da_lista = data.decode("utf-8")
+		lista_de_linhas = json.loads(json_da_lista)
+		linhas = '\n'.join(lista_de_linhas)
+		arquivo = open('file2.txt', 'w')
+		arquivo.writelines(linhas)
+		arquivo.close()
+except:
+	print("Um erro de conexão ocorreu! \n -Servidor desligado \n ou -Endereço IP e porta incorretos ")
